aadd673c3a214f07278b2ab9f1cf01ba1610e721
schultzberg <viktor.schultzberg@gmail.com>
2016-01-20 11:11:40 +0100

    cleaned up SQL and solved css issues

--- Code/classes/User.class.php
+++ Code/classes/User.class.php
@@ -101,7 +101,7 @@ class User{
 			$uniqueUrl = $params[0];
 			$uniqueUrlClean = $mysqli->real_escape_string($uniqueUrl);
 
-			return ['guestListItems' => Sql::listItemsGuest($uniqueUrlClean), 'guestBlackListItems' => Sql::listBlackItemsGuest($uniqueUrlClean), 'listName' => Sql::getListName($uniqueUrl), 'listSubNames'=> Sql::getListSubName($uniqueUrl), 'userPermission' => Sql::getUserGuestPermission($uniqueUrlClean),'imageUrl' => Sql::getListImage($uniqueUrlClean)];
+			return ['guestListItems' => Sql::listItemsGuest($uniqueUrlClean), 'guestBlackListItems' => Sql::listBlackItemsGuest($uniqueUrlClean), 'userPermission' => Sql::getUserGuestPermission($uniqueUrlClean),'imageUrl' => Sql::getListImage($uniqueUrlClean)];
 		}
 
 		public static function itemDone($params) {
